52ff2b1ec62476a76507b263b04628dd8a662827
GraspinglySilver <jack@jackmagiera.com>
2014-02-18 18:35:05 -0500

    began basic collection view functionality

---
+++ Earthquake/EarthquakeCollectionCell.m
@@ -0,0 +1,31 @@
+//
+//  EarthquakeCollectionCell.m
+//  Earthquake
+//
+//  Created by Jack Magiera on 2/18/14.
+//  Copyright (c) 2014 Yuxuan Chen. All rights reserved.
+//
+
+#import "EarthquakeCollectionCell.h"
+
+@implementation EarthquakeCollectionCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
